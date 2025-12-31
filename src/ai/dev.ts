e0b25c8c442bfb67f65130165b9f83a4233dc327
Vedik Prasad <vedikprasad@gmail.com>
2025-12-31 09:09:10 +0000

    Try fixing this error: `Console Error: Failed to fetch from https://gene

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -1,5 +1,6 @@
+'use client';
 import { config } from 'dotenv';
 config();
 
+import '@/ai/flows/classify-waste.ts';
 import '@/ai/flows/summarize-classification-history.ts';
-import '@/ai/flows/classify-waste';
